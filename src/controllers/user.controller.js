1eaa1302b5bb1415f2b4098e4a98cd25fcb60823
Debadyuti Karmakar <debadyuti.karmakar@gmail.com>
2024-05-24 17:29:57 +0530

    logged out

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -125,4 +125,27 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, loginUser };
+//logout user
+const logoutUser = asyncHandler(async (req, res) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: {
+        refreshToken: 1,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const options ={
+    httpOnly:true,
+    secure:true
+  }
+
+  return res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options).json(
+    new ApiResponse(200,{},"logout successfully")
+  )
+});
+
+export { registerUser, loginUser, logoutUser };
